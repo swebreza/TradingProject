9e8b1c788c691027ab8896731e6c0a9af3c24eb9
MD Suweb Reza <45898572+swebreza@users.noreply.github.com>
2022-09-18 13:53:10 +0530

    JSON

--- TradingProject/MainApp/urls.py
+++ TradingProject/MainApp/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.Data),
+    path('json',views.BuildJson,name='BuildJson'),
     # path('admin/', admin.site.urls),
 ]
